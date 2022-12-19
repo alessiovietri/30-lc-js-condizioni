2a31c137d3b31f63820d2b6201294b6b3dbcf580
Alessio Vietri <info@alessiovietri.it>
2022-12-19 12:26:48 +0100

    Completato LC 3

---
+++ 3_date/js/scripts.js
@@ -0,0 +1,14 @@
+const now = new Date();
+console.log('now', now);
+
+now.getHours() //Get the hour (0-23)
+console.log('now.getHours()', now.getHours());
+
+now.getMinutes() // Get the minute (0-59)
+console.log('now.getMinutes()', now.getMinutes());
+
+now.getDay() // Get the day as number (0-6)
+console.log('now.getDay()', now.getDay());
+
+now.getFullYear() // Get Year as 2019
+console.log('now.getFullYear()', now.getFullYear());
